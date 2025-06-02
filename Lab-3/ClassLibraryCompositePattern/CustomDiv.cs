5f0de23edacb99f636b686c037928bb328675489
Anna Svintsitska <ipz233_saye@student.ztu.edu.ua>
2025-06-02 18:23:02 +0300

    Add Template Method lifecycle hooks for HTML elements

---
+++ Lab-3/ClassLibraryCompositePattern/CustomDiv.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryCompositePatternAndFlyweight
+{
+    public class CustomDiv : LifecycleElementNode
+    {
+        public CustomDiv() : base("div", DisplayType.Block, ClosingType.WithClosingTag)
+        {
+        }
+
+        public override void OnCreated()
+        {
+            Console.WriteLine("CustomDiv: OnCreated");
+        }
+
+        public override void OnInserted()
+        {
+            Console.WriteLine("CustomDiv: OnInserted");
+        }
+    }
+}
--- Lab-3/ClassLibraryCompositePattern/LightElementNode.cs
+++ Lab-3/ClassLibraryCompositePattern/LightElementNode.cs
@@ -83,6 +83,4 @@ public class LightElementNode : LightNode
             return sb.ToString();
         }
     }
-
-
 }
